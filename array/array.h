12cde5a3079d4205465842ab6e21f1041bd1174e
Darshan Ghumare <darshanghumare@Darshans-iMac.local>
2015-10-19 10:52:41 +0530

    Array
    Updated Array,h with declaration of functions added in previous commit.

--- array/array.h
+++ array/array.h
@@ -3,5 +3,8 @@
 #define  __ARRAY_H__
 
 int rotated_array_bsearch(int a[], int key, int low, int up);
+void insertion_sort_right_to_left(int a[], int length);
+void selection_sort(int a[], int length);
+void array_merge(int a[], int p, int b[], int q, int c[]);
 
 #endif // __ARRAY_H__
