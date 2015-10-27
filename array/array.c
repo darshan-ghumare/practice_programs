53b2fe6261b5ccfde6441731863c43baf496fcd0
Darshan Ghumare <darshanghumare@Darshans-iMac.local>
2015-10-27 10:21:32 +0530

    Array
    Min-heap, max-heap and find subarray with maximum sum from CLR.

--- array/array.c
+++ array/array.c
@@ -177,4 +177,284 @@ void array_merge(int a[], int p, int b[], int q, int c[])
     }
 }
 
+/*
+ * Given an array find a subarray such that sum of all its elements is maximum among all subarray.
+ * This takes O(n^2) time, in CLR ch 2 same is solved with O(n log n).
+ *
+ * @a[] : Array of ints.
+ # @length : Number of elements in the above array.
+ * @max_sub_array_start : Starting index of max subarray found as output param.
+ * @max_subarray_end : Ending index of max subarray found as output param.
+ * @max_subarray : Sum of all elements of max subarray as output param.
+ */
+void array_find_max_subarray_n2(int a[], int length, int *max_sub_array_start, int *max_subarray_end, int *max_subarray)
+{
+    if (length == 1)
+    {
+        *max_sub_array_start = *max_subarray_end = 0;
+        *max_subarray = a[0];
+        return 0;
+    }
+
+    //Init, also n > 1.
+    *max_subarray = a[0];
+    *max_subarray_start = *max_subarray_end = 0;
+
+    for (i = 0; i < n; i++)
+    {
+        //Init before finding max subarray at ith index.
+        ith_max_subarray = a[i];
+
+        for (j = i + 1; j < n; j++)
+        {
+            ith_max_subarray += a[j];
+            if (ith_max_subarray > *max_subarray)
+            {
+                *max_subarray = ith_max_subarray;
+                *max_subarray_start = i;
+                *max_subarray_end = j;
+            }
+        }
+    }
+}
+
+/*
+ * Given an array find a subarray such that sum of all its elements is maximum among all subarray.
+ * This takes O(n^2) time, in CLR ch 2 same is solved with O(n log n).
+ *
+ * @a[] : Array of ints.
+ # @length : Number of elements in the above array.
+ * @max_sub_array_start : Starting index of max subarray found as output param.
+ * @max_subarray_end : Ending index of max subarray found as output param.
+ * @max_subarray : Sum of all elements of max subarray as output param.
+ */
+void array_find_max_subarray_nlogn(int a[], int length, int *max_sub_array_start, int *max_subarray_end, int *max_subarray)
+{
+    if (length == 1)
+    {
+        *max_sub_array_start = *max_subarray_end = 0;
+        *max_subarray = a[0];
+        return 0;
+    }
+
+    //Init, also n > 1.
+    *max_subarray = a[0];
+    *max_subarray_start = *max_subarray_end = 0;
+
+    for (j = 1; j < n; j++)
+    {
+        ith_max_subarray += a[j];
+        if (ith_max_subarray > *max_subarray)
+        {
+            *max_subarray = ith_max_subarray;
+            *max_subarray_start = i;
+            *max_subarray_end = j;
+        }
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        //Init before finding max subarray at ith index.
+        ith_max_subarray = a[i];
+
+        for (j = i + 1; j < n; j++)
+        {
+            ith_max_subarray += a[j];
+            if (ith_max_subarray > *max_subarray)
+            {
+                *max_subarray = ith_max_subarray;
+                *max_subarray_start = i;
+                *max_subarray_end = j;
+            }
+        }
+    }
+}
+
+/*
+ * Build max-heap from ith node in the heap.
+ * Heap starts from a[1].
+ * Retuns 0 on success and 1 on failure.
+ *
+ * @a : Array of ints represents max-heap.
+ * @i : Starting element to buid max-heap.
+ * @n : Size of the max-heap.
+ */
+
+#define HEAP_LEFT_CHILD(X)      (2U * (X))
+#define HEAP_RIGHT_CHILD(X)     ((2U * (X)) + 1)
+#define HEAP_PARENT(X)          ((X) / 2)
+
+int array_max_heapify(int a[], unsigned i, unsigned n)
+{
+    unsigned left_child = 0, right_child = 0, largest = 0;
+    unsigned temp = 0;
+
+    if (i > n)
+    {
+        return 1;
+    }
+
+    while (i < n)
+    {
+        left_child = HEAP_LEFT_CHILD(i);
+        right_child = HEAP_RIGHT_CHILD(i);
+
+        //Find largest among left_child, right_child of ith element and ith element itself.
+        largest = (left_child <= n && a[left_child] > a[i])? left_child: i;
+        largest = (right_child <= n && a[right_child] > a[largest])? right_child: largest;
+
+        if (i == largest)
+        {
+            //Done.
+            break;
+        }
+
+        //Swap ith element with largest element to satisfy heap property.
+        temp = a[i];
+        a[i] = a[largest];
+        a[largest] = temp;
+
+        //Re-max-heapify from largest element, new ith element.
+        i = largest;
+    }
+
+    return 0;
+}
+
+/*
+ * Build min-heap from ith node in the heap.
+ * Heap starts from a[1].
+ * Retuns 0 on success and 1 on failure.
+ *
+ * @a : Array of ints represents min-heap.
+ * @i : Starting element to buid min-heap.
+ * @n : Size of the max-heap.
+ */
+int array_min_heapify(int a[], unsigned i, unsigned n)
+{
+    unsigned left_child = 0, right_child = 0, smallest = 0;
+    unsigned temp = 0;
+
+    if (i > n)
+    {
+        return 1;
+    }
+
+    while (i < n)
+    {
+        left_child = HEAP_LEFT_CHILD(i);
+        right_child = HEAP_RIGHT_CHILD(i);
+
+        //Find smallest among left_child, right_child of ith element and ith element.
+        smallest = (left_child <= n && a[left_child] < a[i])? left_child: i;
+        smallest = (right_child <= n && a[right_child] < a[smallest])? right_child: smallest;
+
+        if (i == smallest)
+        {
+            //Done.
+            break;
+        }
+
+        //Swap ith element with smallest element to satisfy heap property.
+        temp = a[i];
+        a[i] = a[smallest];
+        a[smallest] = temp;
+
+        //Re-min-heapify from smallest element, new ith element.
+        i = smallest;
+    }
+
+    return 0;
+}
+
+/*
+ * Build min-heap..
+ *
+ * @a: Array of ints representing min-heap.
+ * @: Size of the min-heap.
+ */
+void array_build_min_heap(int a[], unsigned n)
+{
+    assert(n > 0);
+
+    /*
+     * For any i, 0 > i <= n, is a leaf node if i >= n/2 otherwise its childrens must at 2 * i, 2 * i <= n
+     * i.e. (n /2 )th node is the last parent in the heap wich has atleast one child.
+     * By above theorem n / 2 must have atlest one child which is a leaf node, this is starting point, and
+     * (n / 2) + 1 must be a leaf node.
+     */
+    for (i = (n / 2); i > 1; i--)
+    {
+        assert(array_min_heapify(a, i, n) == 0);
+    }
+}
+
+/*
+ * Remove topmost element from Min-Heap then Re-heapify it to put next smallest element at the top.
+ * Heap starts from a[1].
+ * Retuns smallest element from the heap.
+ *
+ * @a: Array of ints representing min-heap.
+ * @: Size of the min-heap.
+ */
+int array_extract_min(int a[], unsigned n)
+{
+    int smallest = 0;
+
+    assert(n > 0)
+
+    smallest = a[1];
+    a[1] = a[n];
+    n--;
+    array_build_min_heap(a, n);
+
+    return smallest;
+}
+
+/*
+ * Decrease value of existing key and then re-heapify min-heap.
+ * After decreasing key then all element before it may or may not be <= new key but
+ * all elements after it must be >= new key value so we just need to re-heapify from 1 to key.
+ * Returns 0 on success and 1 on failure.
+ *
+ * @a : Array on ints represents min-heap.
+ * @i : ith element in min-heap.
+ * @new_key : New key value for ith element.
+ * @n : Size of min-heap.
+ */
+int array_decrease_key(int a[], unsigned i, unsigned new_key, unsigned n)
+{
+    if (i > n ||  n == 0 || new_key >= a[i])
+    {
+        return 1;
+    }
+
+    a[i] = new_key;
+
+    while (i > 1 && a[HEAP_PARENT(i)] > a[i])
+    {
+        temp = a[HEAP_PARENT(i)];
+        a[HEAP_PARENT(i)] = a[i];
+        a[i] = temp;
+        i = HEAP_PARENT(i);
+    }
+
+    return 0;
+}
+/*
+ * Insert new key into the min-heap.
+ * Increase the size of the heap and add new key as last element then re-heapify from n to 1.
+ *
+ * @a : Array on ints represents min-heap.
+ * @new_key : New element.
+ * @n : Size of min-heap.
+ */
+int array_min_heap_insert(int a[], unsigned n, int new_key)
+{
+    n++;
+    a[n] = new_key;
+    array_build_min_heap(a, n);
+
+    return 0;
+}
 
--- array/array.h
+++ array/array.h
@@ -6,5 +6,13 @@ int rotated_array_bsearch(int a[], int key, int low, int up);
 void insertion_sort_right_to_left(int a[], int length);
 void selection_sort(int a[], int length);
 void array_merge(int a[], int p, int b[], int q, int c[]);
+void array_find_max_subarray_n2(int a[], int length, int *max_sub_array_start, int *max_subarray_end, int *max_subarray);
+void array_find_max_subarray_nlogn(int a[], int length, int *max_sub_array_start, int *max_subarray_end, int *max_subarray);
+int array_max_heapify(int a[], unsigned i, unsigned n);
+int array_min_heapify(int a[], unsigned i, unsigned n);
+void array_build_min_heap(int a[], unsigned n);
+int array_extract_min(int a[], unsigned n);
+int array_decrease_key(int a[], unsigned i, unsigned new_key, unsigned n);
+int array_min_heap_insert(int a[], unsigned n, int new_key);
 
 #endif // __ARRAY_H__
