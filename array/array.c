939e1e1b382c60f235c6332bf92d847b89c7d34e
Darshan Ghumare <darshan@Darshans-MacBook-Pro.local>
2015-12-16 09:38:05 +0530

    Array based problem from GeeksforGeeks.

--- array/array.c
+++ array/array.c
@@ -177,4 +177,75 @@ void array_merge(int a[], int p, int b[], int q, int c[])
     }
 }
 
+/*
+ * Find three closest elements from given three sorted arrays
+ * Given three sorted arrays A[], B[] and C[], find 3 elements i, j and
+ * k from A, B and C respectively such that max(abs(A[i] – B[j]), abs(B[j] – C[k]),
+ * abs(C[k] – A[i])) is minimized. Here abs() indicates absolute value.
+ *
+ *  Example :
+ *
+ *  Input: A[] = {1, 4, 10}
+ *  B[] = {2, 15, 20}
+ *  C[] = {10, 12}
+ *  Output: 10 15 10
+ *  10 from A, 15 from B and 10 from C
+ *
+ *  Input: A[] = {20, 24, 100}
+ *  B[] = {2, 19, 22, 79, 800}
+ *  C[] = {10, 12, 23, 24, 119}
+ *  Output: 24 22 23
+ *  24 from A, 22 from B and 23 from C
+ *
+ *  Source : http://www.geeksforgeeks.org/find-three-closest-elements-from-given-three-sorted-arrays/
+ *
+ *  Algo:
+ *  1) Start with i=0, j=0 and k=0 (Three index variables for A,
+ *     B and C respectively)
+ *
+ *  //  p, q and r are sizes of A[], B[] and C[] respectively.
+ *  2)  Do following while i < p and j < q and k < r
+ *      a) Find min and maximum of A[i], B[j] and C[k]
+ *      b) Compute diff = max(X, Y, Z) - min(A[i], B[j], C[k]).
+ *      c) If new result is less than current result, change 
+ *      it to the new result.
+ *      d) Increment the pointer of the array which contains 
+ *      the minimum.
+ */
+void findClosest(int A[], int B[], int C[], int p, int q, int r)
+{
+
+    int diff = INT_MAX;  // Initialize min diff
+
+    // Initialize result
+    int res_i =0, res_j = 0, res_k = 0;
+
+    // Traverse arrays
+    int i=0,j=0,k=0;
+    while (i < p && j < q && k < r)
+    {
+        // Find minimum and maximum of current three elements
+        int minimum = min(A[i], min(B[j], C[k]));
+        int maximum = max(A[i], max(B[j], C[k]));
+
+        // Update result if current diff is less than the min
+        // diff so far
+        if (maximum-minimum < diff)
+        {
+            res_i = i, res_j = j, res_k = k;
+            diff = maximum - minimum;
+        }
+
+        // We can't get less than 0 as values are absolute
+        if (diff == 0) break;
+
+        // Increment index of array with smallest value
+        if (A[i] == minimum) i++;
+        else if (B[j] == minimum) j++;
+        else k++;
+    }
+
+    // Print result
+    cout << A[res_i] << " " << B[res_j] << " " << C[res_k];
+}
 
